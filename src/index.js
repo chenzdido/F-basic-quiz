b4a4cf75ff416f3835b600b2682372e6f222d2fc
chenzdido <chenzdido@gmail.com>
2020-09-15 17:29:00 +0800

    [zhe] feat: complete main section.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const logo = document.getElementById("logo");
 const age = document.getElementById("age");
 const name = document.getElementById("name");
 const description = document.getElementById("description");
+const edulist = document.getElementById("edulist");
 
 const userdata = () =>
   fetch("http://localhost:8080/users/1", {
@@ -17,11 +18,33 @@ const userdata = () =>
     .then((data) => {
       return data;
     });
+const educations = () =>
+  fetch("http://localhost:8080/users/1/educations", {
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "user-agent": "Mozilla/4.0 MDN Example",
+      "content-type": "application/json",
+    },
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
 let user;
+let edu;
 window.onload = async () => {
   user = await userdata();
+  edu = await educations();
   logo.src = user.imgURL;
   age.innerText = user.age;
   name.innerText = user.name;
   description.innerText = user.description;
+  let a = "";
+  for (let i = 0; i < edu.length; ) {
+    a += `<li><div>${edu[i].year}<div><div><p>${edu[i].description}<p><p>${edu[i].title}<p><div></li>`;
+    i += 1;
+  }
+  edulist.innerHTML = a;
 };
